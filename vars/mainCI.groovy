0690e77f1af8f38ae05fb0f3f26eef43f51d24a3
Akhil <akhilmahendra035@gmail.com>
2024-12-23 18:47:16 +0530

    Update CI pipeline

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -10,17 +10,17 @@ def call() {
             }
             checkout scmGit(
                     branches: [[name: "${branch_name}"]],
-                    userRemoteConfigs: [[url: "https://github.com/devops-i1/expense-${component}"]]
+                    userRemoteConfigs: [[url: "https://github.com/devops-i1/roboshop-${component}"]]
             )
         }
 
         if (env.TAG_NAME ==~ '.*') {
             stage('Build Code') {
-                sh 'docker build -t 835817189095.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} .'
+                sh 'docker build -t 835817189095.dkr.ecr.us-east-1.amazonaws.com/roboshop-${component}:${TAG_NAME} .'
             }
             stage('Release Software') {
                 sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 835817189095.dkr.ecr.us-east-1.amazonaws.com'
-                sh 'docker push 835817189095.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
+                sh 'docker push 835817189095.dkr.ecr.us-east-1.amazonaws.com/roboshop-${component}:${TAG_NAME}'
             }
             stage('Deploy to Dev') {
                 sh 'aws eks update-kubeconfig --name dev-eks'
